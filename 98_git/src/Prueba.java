3c26d5e526638269fe91576589cf1c02ea050149
elena <Curso maana@PC08a5>
2018-01-29 12:59:46 +0100

    cambio de elena

--- 98_git/src/Prueba.java
+++ 98_git/src/Prueba.java
@@ -4,6 +4,7 @@ public class Prueba {
 	public static void main(String[] args) {
 		System.out.println("prueba modificada");
 		System.out.println("primer cambio");
+		System.out.println("segundo cambio");
 	}
 
 }
